f4f466483daf814c4f1c26a86b4440003de1f841
Nils Naumann <nils.naumann@sodex.at>
2024-09-16 20:02:38 +0200

    version inc

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="pymongex",
-    version="0.0.2",
+    version="0.0.3",
     author="Nils Naumann",
     author_email="nils.naumann02@gmail.com",
     packages=find_packages(),
